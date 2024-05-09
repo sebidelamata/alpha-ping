9ad77d1bbcae7498bc18e09661f01f6c12dec664
Miguel  de la Mata <sebidelamata@gmail.com>
2024-05-09 11:37:24 -0600

    built router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import * as React from "react";
+import {createRoot} from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./Routes";
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(routes);
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
