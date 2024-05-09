1219a6784155ec1e22034140c6e052da6e24bff9
Miguel  de la Mata <sebidelamata@gmail.com>
2024-05-09 11:53:30 -0600

    changed svg logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='app-container'>
+      <div className='logo-container'>
+        <img src="/Apes.svg" alt="AlphaPING Logo" />
       </div>
-      <h1>Test</h1>
+      <h1>AlphaPING</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,7 +21,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
