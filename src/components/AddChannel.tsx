6522b68f057236a419ec84cb40af0308816b9e61
Miguel  de la Mata <sebidelamata@gmail.com>
2024-05-13 14:27:09 -0600

    add channel form styles

--- src/components/AddChannel.tsx
+++ src/components/AddChannel.tsx
@@ -1,4 +1,7 @@
-import React, {useState} from "react";
+import React, {
+    useState, 
+    MouseEventHandler
+} from "react";
 import { AlphaPING } from '../../typechain-types/contracts/AlphaPING.sol/AlphaPING';
 import {ethers} from 'ethers'
 
@@ -26,50 +29,63 @@ const AddChannel:React.FC<AddChannelProps> = ({alphaPING, provider}) => {
         const signer:any = await provider?.getSigner()
         let transaction = await alphaPING?.connect(signer).createChannel(tokenAddress,tokenType)
         await transaction?.wait()
+
+        setShowAddChannelModal(false)
+        setTokenAddress("")
+        setTokenType("ERC20")
+
     }
 
     return(
         <>
             <div className="add-channel">
-            <button 
-                className="add-channel-button"
-                onClick={() => addChannelModal()}>+ Channel</button>
+                <button 
+                    className="add-channel-button"
+                    onClick={() => addChannelModal()}>+ Channel
+                </button>
             </div>
             {
                 showAddChannelModal === true &&
-                <div className="add-channel-modal">
-                    <h2 className="add-channel-title">
-                        Add Channel
-                    </h2>
-                    <p className="add-channel-text">
-                        Enter the address of any token (ERC-20) or NFT (ERC-721) to create a new channel.
-                    </p>
-                    <form action="" className="add-channel-form" onSubmit={(e) => createChannel(e)}>
-                        <label htmlFor="address">Address</label>
-                        <input 
-                            type="text" 
-                            name="address" 
-                            placeholder="0x..." 
-                            value={tokenAddress} 
-                            onChange={(e) => setTokenAddress(e.target.value)} 
-                        />
-                        <label htmlFor="type">Address</label>
-                        <select 
-                            className="token-type-selector" 
-                            name="type"
-                            onChange={(e) => setTokenType(e.target.value)}
-                            defaultValue="ERC20"
-                        >
-                            <option value="ERC20">ERC-20</option>
-                            <option value="ERC721">ERC-721</option>
-                        </select>
-                        <button 
-                            className="add-channel-button"
-                            type="submit"
-                        >
-                            Create
-                        </button>
-                    </form>
+                <div className="add-channel-greyout">
+                    <div className="add-channel-modal">
+                        <h2 className="add-channel-title">
+                            Add Channel
+                        </h2>
+                        <h4 className="add-channel-text">
+                            Enter the address of any token (ERC-20) or NFT (ERC-721).
+                        </h4>
+                        <form action="" className="add-channel-form" onSubmit={(e) => createChannel(e)}>
+                            <div className="form-line-one">
+                                <label htmlFor="address">Address</label>
+                                <input 
+                                    type="text" 
+                                    name="address" 
+                                    placeholder="0x..." 
+                                    value={tokenAddress} 
+                                    onChange={(e) => setTokenAddress(e.target.value)} 
+                                />
+                                <label htmlFor="type">Token Type</label>
+                                <select 
+                                    className="token-type-selector" 
+                                    name="type"
+                                    onChange={(e) => setTokenType(e.target.value)}
+                                    defaultValue="ERC20"
+                                    value={tokenType}
+                                >
+                                    <option value="ERC20">ERC-20</option>
+                                    <option value="ERC721">ERC-721</option>
+                                </select>
+                            </div>
+                            <div className="add-channel-button-container">
+                                <button 
+                                    className="add-channel-button"
+                                    type="submit"
+                                >
+                                    <h3>Create</h3>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
 
             }
