e851ef35700d43679ecf1e985906d1089c86c1e3
Miguel  de la Mata <sebidelamata@gmail.com>
2024-09-28 14:30:54 +0100

    delete message test

--- src/components/DeleteMessage.tsx
+++ src/components/DeleteMessage.tsx
@@ -5,9 +5,23 @@ import React, {
 import { AlphaPING } from "../../typechain-types/contracts/AlphaPING.sol/AlphaPING";
 
 const DeleteMessage:React.FC = () => {
+
+    const messageDelete = useState(false)
+    const messageDeleteLoading = useState(false)
+    const messageDeleteError = useState(false)
+
+    const handleClick = () => {
+        console.log('click')
+    }
+
     return(
         <div className="delete-message">
-            Test
+            <button 
+                className="delete-message-button"
+                onClick={() => handleClick()}
+            >
+                Test
+            </button>
         </div>
     )
 }
