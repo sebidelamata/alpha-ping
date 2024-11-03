5148cac1e6d6dacaa3afb5646a50e2ec4d749b34
Miguel  de la Mata <sebidelamata@gmail.com>
2024-11-02 18:50:06 -0600

    owner tab can unban users on blacklist list

--- src/components/Profile/BlacklistList.tsx
+++ src/components/Profile/BlacklistList.tsx
@@ -1,15 +1,83 @@
-import React from "react";
+import React, {
+    useState,
+    useEffect
+} from "react";
+import { useEtherProviderContext } from "../../contexts/ProviderContext";
+import BlacklistListItem from "./BlacklistListItem";
 
 const BlacklistList:React.FC = () => {
+
+    const { alphaPING } = useEtherProviderContext()
+
+    //hold tx message for a ban
+    const [txMessageUnblacklist, setTxMessageUnblacklist] = useState<string | null | undefined>(null)
+
+    const [allUsers, setAllUsers] = useState<string[]>([])
+    const fetchAllUsers = async() => {
+        try{
+            const totalUsers = await alphaPING?.totalSupply() || 0
+            const allUsers = []
+            for(let i=1; i<=totalUsers; i++){
+                const address = await alphaPING?.ownerOf(i)
+                if(address !== undefined){
+                    allUsers.push(address)
+                }
+            }
+            setAllUsers(allUsers)
+        }catch(error){
+            console.error(error)
+        }
+        
+    }
+    useEffect(() => {
+        fetchAllUsers()
+    }, [])
+
+    const [blacklistedUsers, setBlacklistedUsers] = useState<string[]>([])
+    const fetchBlacklistedUsers = async () => {
+        try{
+            const blacklist = []
+            for(let i=0; i<(allUsers?.length || 0); i++){
+                const result = await alphaPING?.isBlackListed(allUsers[i])
+                if(result === true){
+                    blacklist.push(allUsers[i])
+                }
+            }
+            setBlacklistedUsers(blacklist)
+        }catch(error){
+            console.error(error)
+        }
+    }
+    useEffect(() => {
+        fetchBlacklistedUsers()
+    },[allUsers, txMessageUnblacklist])
+
     return(
         <div className="blacklist-list">
             <h3 className="blacklist-list-header">
                 Blacklisted Users
             </h3>
+            {
+                blacklistedUsers &&
+                blacklistedUsers.length === 0 &&
+                <div>
+                    There are currently no Blacklisted Users on AlphaPING
+                </div>
+            }
             <ul className="blacklist-list-list">
-                <li className="blacklist-list-item">
-                    test
-                </li>
+                {
+                    blacklistedUsers &&
+                    blacklistedUsers.length > 0 &&
+                    blacklistedUsers.map((user) => (
+                        <li key={user} className="blacklist-list-item">
+                            <BlacklistListItem 
+                                user={user}
+                                txMessageUnblacklist={txMessageUnblacklist}
+                                setTxMessageUnblacklist={setTxMessageUnblacklist}
+                            />
+                        </li>
+                    ))
+                }
             </ul>
         </div>
     )
---
+++ src/components/Profile/BlacklistListItem.tsx
@@ -0,0 +1,133 @@
+import React, {
+    useState,
+    useEffect,
+    MouseEvent,
+    FormEvent
+} from "react";
+import { useEtherProviderContext } from "../../contexts/ProviderContext";
+import monkey from '/monkey.svg'
+import Loading from "../Loading";
+
+interface BlacklistListItemProps{
+    user: string;
+    txMessageUnblacklist: string | null | undefined; 
+    setTxMessageUnblacklist: React.Dispatch<React.SetStateAction<string | null | undefined>>;
+}
+
+interface ErrorType{
+    message: string;
+}
+
+const BlacklistListItem:React.FC<BlacklistListItemProps> = ({
+    user,
+    txMessageUnblacklist,
+    setTxMessageUnblacklist
+}) => {
+
+    const { alphaPING, signer } = useEtherProviderContext()
+
+    const [showModal, setShowModal] = useState<boolean>(false)
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<string | null>(null)
+
+    const handleClick = (e:MouseEvent) => {
+        e.preventDefault()
+        setShowModal(true)
+    }
+
+    const handleCancel = (e:MouseEvent) => {
+        e.preventDefault()
+        setShowModal(false)
+    }
+
+    const handleSubmit = async (e:FormEvent) => {
+        e.preventDefault()
+        setError(null)
+        setTxMessageUnblacklist(null)
+        setLoading(true)
+        try{
+            if(user && user !== undefined){
+                const tx = await alphaPING?.connect(signer).unBlacklistUser(user)
+                await tx?.wait()
+                setTxMessageUnblacklist(tx?.hash)
+            }
+        }catch(error: unknown){
+            if((error as ErrorType).message)
+            setError((error as ErrorType).message)
+        }finally{
+            setLoading(false)
+        }
+
+    }
+
+    const [username, setUsername] = useState<string | null>(null)
+    const [userPFP, setUserPFP] = useState<string | null>(null)
+    const fetchUserMetaData = async () => {
+        try{
+            const usernameResult = await alphaPING?.username(user) || null
+            setUsername(usernameResult)
+            const pfpResult = await alphaPING?.profilePic(user) || null
+            setUserPFP(pfpResult)
+        }catch(error){
+            console.error(error)
+        }
+    }
+    useEffect(() => {
+        fetchUserMetaData()
+    }, [user])
+
+    return(
+        <div className="blacklisted-user-container">
+            <div className="blacklisted-pfp">
+                {
+                    (userPFP !== null && userPFP !== '') ?
+                    <img src={userPFP} alt="User Icon" className='monkey-icon'/> :
+                    <img src={monkey} alt="User Icon" className='monkey-icon'/>
+                }
+            </div>
+            <div className="blacklisted-username">
+                {
+                    (username !== null && username !== '') ?
+                    username :
+                    user.slice(0, 6) + '...' + user.slice(38, 42)
+                }
+            </div>
+            {
+                showModal === false &&
+                    <button
+                        onClick={(e) => handleClick(e)}
+                        className="blacklist-pardon-button"
+                    >
+                        Pardon User
+                    </button>
+            }
+            {
+                showModal === true &&
+                <form 
+                    action=""
+                    onSubmit={(e) => handleSubmit(e)}
+                    className="pardon-form"
+                >
+                    <input 
+                        type="submit" 
+                    />
+                    <button 
+                        onClick={(e) => handleCancel(e)}
+                    >
+                        Cancel
+                    </button>
+                </form>
+            }
+            {
+                loading === true &&
+                    <Loading/>
+            }
+            {
+                error !== null &&
+                    <p>{error}</p>
+            }
+        </div>
+    )
+}
+
+export default BlacklistListItem
