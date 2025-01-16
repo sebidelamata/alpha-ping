a41cbcb0063500c023b41e54e2deb6b9052273cf
Miguel  de la Mata <sebidelamata@gmail.com>
2025-01-15 20:16:13 -0500

    docs sidebar rough

--- src/app/docs/components/DocsSidebar.tsx
+++ src/app/docs/components/DocsSidebar.tsx
@@ -19,6 +19,8 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
   } from "@/components/components/ui/sidebar"
+  import React from "react"
+  import Link from "next/link"
    
   // Menu items.
   const items = [
@@ -76,12 +78,17 @@ import {
           <SidebarGroup className="gap-14">
             <SidebarGroupLabel>
                 <h1 className="text-xl text-secondary">AlphaPING Docs</h1>
-                <img 
-                    src="../Apes.svg" 
-                    alt="AlphaPING Logo" 
-                    className="grid size-12 justify-center object-contain align-middle" 
-                    loading="lazy" 
-                />
+                <Link 
+                    href={'https://alphaping.xyz'} 
+                    target="_blank"
+                >
+                    <img 
+                        src="../Apes.svg" 
+                        alt="AlphaPING Logo" 
+                        className="grid size-12 justify-center object-contain align-middle" 
+                        loading="lazy" 
+                    />
+                </Link>
             </SidebarGroupLabel>
             <SidebarGroupContent>
               <SidebarMenu>
--- src/app/docs/components/Overview.tsx
+++ src/app/docs/components/Overview.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Overview:React.FC = () => {
     return(
-        <div className="overview-container">
-            <h2 className="overview-title">
+        <div className="relative grid grid-rows-[10%_90%] overflow-y-clip">
+            <h2 className="pl-8 text-4xl">
                 Overview
             </h2>
-            <div className="overview-body">
+            <div className="overflow-y-scroll p-8 pt-16">
                 <p>
                     AlphaPING is an aggregated social trading platform that combines real-time chat, data analysis, project research, and trading in a single application. It is designed to foster a transparent and engaging community of traders, researchers, and enthusiasts, while filtering out spam and bots. Users gain access by minting a ERC-721 membership token. Token holdings and balances are recorded and displayed alongside each user’s posts, helping establish transparency and discouraging fraudulent activities. Integrated with the 1Inch Router, AlphaPING ensures users can execute trades at optimal rates without needing to exit the platform.
                 </p>
