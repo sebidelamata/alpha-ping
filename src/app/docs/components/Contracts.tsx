1c827044803147a1ec2575bf7fa21edd7f7662f5
Miguel  de la Mata <sebidelamata@gmail.com>
2025-01-19 08:17:31 -0500

    contracts tailwind

--- src/app/docs/components/Contracts.tsx
+++ src/app/docs/components/Contracts.tsx
@@ -1,20 +1,28 @@
 import React from "react";
+import Link from "next/link";
 
 const Contracts:React.FC = () => {
     return(
-        <div className="contracts-body">
-            <h2>Contracts</h2>
-            <ul className="contracts-list">
-                <li className="contracts-list-item">
-                    <p>
-                        AlphaPING Contract -  
-                             <a 
+        <div className="relative flex h-screen flex-col gap-4">
+            <h2 className="p-4 text-4xl font-bold">
+                Contracts
+            </h2>
+            <ul className="px-4">
+                <li className="flex flex-col gap-1 p-2 text-xl">
+                    <h3>Arbitrum</h3>
+                    <ul className="px-4">
+                        <li className="flex gap-1 p-2 text-lg">
+                            <p>
+                                AlphaPING Contract -  
+                            </p>
+                            <Link
                                 href={`https://arbiscan.io/address/${process.env.NEXT_PUBLIC_ALPHAPING_CONTRACT_ADDRESS}`}
                                 target="_blank"
                             >
-                               <strong>{process.env.NEXT_PUBLIC_ALPHAPING_CONTRACT_ADDRESS}</strong>
-                            </a>
-                    </p>
+                                <strong>{process.env.NEXT_PUBLIC_ALPHAPING_CONTRACT_ADDRESS}</strong>
+                            </Link>
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </div>
