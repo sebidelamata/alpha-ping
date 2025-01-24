94abba0f7e16011c4d066ef4c3e3159fb6a5bc7e
Miguel  de la Mata <sebidelamata@gmail.com>
2025-01-23 20:01:10 -0500

    blacklisted shadcn

--- src/app/app/components/BlacklistedScreen.tsx
+++ src/app/app/components/BlacklistedScreen.tsx
@@ -1,29 +1,44 @@
 import React from "react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/components/ui/dialog"
+import { Separator } from "@radix-ui/react-separator";
 
 const BlacklistedScreen:React.FC= () => {
 
     return(
-        <div className='join-alpha-ping'>
-          <div className='join-container'>
-            <h2 className="join-header">
-                Access Denied
-            </h2>
-            <div className="join-icon">
-                <img 
-                    src="../Apes.svg" 
-                    alt="AlphaPING Logo" 
-                    className="join-logo"
-                    loading="lazy"
-                />
-            </div>
-            <h3 className="join-body">
-                You have been Blacklisted from AlphaPING 
-            </h3>
-            <div className="join-button-container">
-                Contact Admin for Support
-            </div>
-          </div>
-        </div>
+        <Dialog open={true}>
+            <DialogContent className="items-center justify-center align-middle">
+                <DialogHeader className="items-center justify-center align-middle">
+                    <DialogTitle className="items-center justify-center align-middle text-3xl">
+                        Access Denied
+                    </DialogTitle>
+                    <Separator className="h-2"/>
+                    <DialogDescription className="flex flex-col items-center justify-center space-y-4">
+                        <div className="object-contain">
+                            <img 
+                                src="../Apes.svg" 
+                                alt="AlphaPING Logo" 
+                                className="size-80"
+                                loading="lazy"
+                            />
+                        </div>
+                        <Separator className="w-64 border border-secondary"/>
+                        <h3 className="text-lg">
+                            You have been Blacklisted from AlphaPING 
+                        </h3>
+                        <Separator className="h-2"/>
+                        <p>
+                            Contact Admin for Support
+                        </p>
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
     )
 }
 
