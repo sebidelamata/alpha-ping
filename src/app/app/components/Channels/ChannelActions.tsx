726323c22b5335028e13cb6dc50730f2f632d1e3
Miguel  de la Mata <sebidelamata@gmail.com>
2025-01-24 17:07:06 -0500

    profile pic on sidebar

--- src/app/app/components/Channels/ChannelActions.tsx
+++ src/app/app/components/Channels/ChannelActions.tsx
@@ -11,6 +11,11 @@ import {
   SidebarMenu,
   SidebarMenuItem,
 } from "@/components/components/ui/sidebar"
+import { 
+  Avatar,
+  AvatarImage,
+  AvatarFallback
+ } from "@/components/components/ui/avatar";
 
 const ChannelActions: React.FC = () => {
 
@@ -31,106 +36,52 @@ const ChannelActions: React.FC = () => {
 
     return(
       <SidebarGroup className="gap-14 pt-4">
-                    <SidebarGroupLabel>
-                        <h1 className="text-xl text-secondary">
-                            Channel Actions
-                        </h1>
-                    </SidebarGroupLabel>
-                    <SidebarGroupContent>
-                        <SidebarMenu>
-                            {
-                              actions.map((action) => (
-                                  <SidebarMenuItem 
-                                    key={action}
-                                    onClick={(e) => channelActionHandler(e)}
-                                    id={action}
-                                  >
-                                      {action}
-                                  </SidebarMenuItem>
-                              ))
-                            }
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
-        // <div className="channel-actions">
-        //   <h2>Channel Actions</h2>
-        //   <ul className="channel-actions-list">
-        //     <li 
-        //       className= {
-        //         channelAction ==  "chat" ? (
-        //           "channel-action-items channel-action-active"
-        //         ) : (
-        //           "channel-action-items"
-        //         )
-        //       }
-        //       id="chat"
-        //       onClick={(e) => channelActionHandler(e)}
-        //     >
-        //       Chat
-        //     </li>
-        //     <li 
-        //       className= {
-        //         channelAction ==  "analyze" ? (
-        //           "channel-action-items channel-action-active"
-        //         ) : (
-        //           "channel-action-items"
-        //         )
-        //       }
-        //       id="analyze"
-        //       onClick={(e) => channelActionHandler(e)}
-        //     >
-        //       Analyze
-        //     </li>
-        //     <li 
-        //       className= {
-        //         channelAction ==  "trade" ? (
-        //           "channel-action-items channel-action-active"
-        //         ) : (
-        //           "channel-action-items"
-        //         )
-        //       }
-        //       id="trade"
-        //       onClick={(e) => channelActionHandler(e)}
-        //     >
-        //       Trade
-        //     </li>
-        //     <li 
-        //       className= {
-        //         channelAction ==  "profile" ? (
-        //           "channel-action-items channel-action-active"
-        //         ) : (
-        //           "channel-action-items"
-        //         )
-        //       }
-        //       id="profile"
-        //       onClick={(e) => channelActionHandler(e)}
-        //     >
-        //       Profile 
-        //       <div className="edit-profile-icon">
-        //         {
-        //             ( 
-        //                 userProfilePic !== null &&
-        //                 userProfilePic !== "" &&
-        //                 userProfilePic !== undefined 
-        //             ) ?
-        //             <img 
-        //                 src={userProfilePic} 
-        //                 alt="user profile picture" 
-        //                 className="edit-profile-image"
-        //                 loading="lazy"
-        //             /> :
-        //             <img 
-        //                 src="/monkey.svg" 
-        //                 alt="default profile picture" 
-        //                 className="edit-profile-image"
-        //                 loading="lazy"
-        //             />
-        //         }
-        //       </div>
-        //     </li>
-        //   </ul>
-        // </div>
-    )
+        <SidebarGroupLabel>
+            <h1 className="text-xl text-secondary">
+                Channel Actions
+            </h1>
+        </SidebarGroupLabel>
+        <SidebarGroupContent>
+            <SidebarMenu>
+                {
+                  actions.map((action) => (
+                      <SidebarMenuItem 
+                        key={action}
+                        onClick={(e) => channelActionHandler(e)}
+                        id={action}
+                        className="flex flex-row"
+                      >
+                          {action}
+                          {
+                            action === "Profile" &&
+                            userProfilePic !== null &&
+                            userProfilePic !== "" &&
+                            userProfilePic !== undefined &&
+                            <Avatar>
+                              <AvatarImage 
+                                src={userProfilePic} 
+                                alt="user profile picture" 
+                                loading="lazy" 
+                              />
+                            </Avatar>
+                          }
+                          {
+                            action === "Profile" &&
+                            <Avatar>
+                              <AvatarImage 
+                                src="/monkey.svg" 
+                                alt="default profile picture" 
+                                loading="lazy"
+                              />
+                            </Avatar>
+                          }
+                      </SidebarMenuItem>
+                  ))
+                }
+            </SidebarMenu>
+        </SidebarGroupContent>
+    </SidebarGroup>
+  )
 }
 
 export default ChannelActions
