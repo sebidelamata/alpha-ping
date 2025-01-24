c7999c5a2a910b1b2f840ee4b19894297e73bd59
Miguel  de la Mata <sebidelamata@gmail.com>
2025-01-24 11:35:06 -0500

    channels in sidebar

--- src/app/app/components/AppSidebar.tsx
+++ src/app/app/components/AppSidebar.tsx
@@ -5,6 +5,7 @@ import React, {
   useEffect 
 } from "react";
 import { AlphaPING } from '../../../../typechain-types/contracts/AlphaPING.sol/AlphaPING';
+import Channel from "./Channels/Channel";
 import AddChannel from "./Channels/AddChannel";
 import { useEtherProviderContext } from "../../../contexts/ProviderContext";
 import { useChannelProviderContext } from "../../../contexts/ChannelContext";
@@ -22,6 +23,73 @@ import {
   } from "@/components/components/ui/sidebar"
 
 const AppSidebar = () => {
+
+    const { 
+        alphaPING, 
+        channels, 
+        setChannels, 
+        hasJoined, 
+        signer, 
+        setHasJoined 
+      } = useEtherProviderContext()
+      const { 
+        currentChannel, 
+        joinChannelLoading, 
+        channelAction, 
+        setChannelAction  
+      } = useChannelProviderContext()
+
+    const [userChannels, setUserChannels] = useState<AlphaPING.ChannelStructOutput[]>([])
+    const loadUserChannels = ():void => {
+        const userChannels: AlphaPING.ChannelStructOutput[] = []
+        hasJoined.map((joined, index) => {
+        if(joined === true){
+            userChannels.push(channels[index])
+        }
+        })
+        setUserChannels(userChannels)
+    }
+    useEffect(() => {
+        loadUserChannels()
+    }, [channels, joinChannelLoading, hasJoined, signer])
+
+    // weve elevated this state from add channels to make the channels list rerender on add channel
+    const [addChannelLoading, setAddChannelLoadingLoading] = useState<boolean>(false)
+
+    // reload our channels if we get a new one
+      const reloadChannels = async () => {
+        const totalChannels:bigint | undefined = await alphaPING?.totalChannels()
+        const channels = []
+    
+        for (let i = 1; i <= Number(totalChannels); i++) {
+          const channel = await alphaPING?.getChannel(i)
+          if(channel){
+            channels.push(channel)
+          }
+        }
+        setChannels(channels)
+    
+        const hasJoinedChannel = []
+    
+          if(alphaPING !== null && signer !== null){
+            for (let i = 1; i <= Number(totalChannels); i++) {
+              const hasJoined = await alphaPING.hasJoinedChannel(
+                (i as ethers.BigNumberish), 
+                await signer.getAddress()
+              )
+              hasJoinedChannel.push(hasJoined)
+            }
+      
+            setHasJoined(hasJoinedChannel as boolean[])
+          }
+      }
+      useEffect(() => {
+        reloadChannels()
+      }, [currentChannel, joinChannelLoading, signer])
+    
+
+
+
     return(
         <Sidebar collapsible="icon" className="mt-24">
             <SidebarContent className="bg-primary text-secondary">
@@ -33,13 +101,16 @@ const AppSidebar = () => {
                     </SidebarGroupLabel>
                     <SidebarGroupContent>
                         <SidebarMenu>
-                            <SidebarMenuItem key={"test"}>
-                                <SidebarMenuButton asChild>
-                                <a href={"https://www.google.com"}>
-                                    <span>{"test"}</span>
-                                </a>
-                                </SidebarMenuButton>
-                            </SidebarMenuItem>
+                            {
+                                userChannels.map((channel, index) => (
+                                    <SidebarMenuItem key={channel.tokenAddress}>
+                                        <Channel
+                                            channel={channel}
+                                            key={index}
+                                        />
+                                    </SidebarMenuItem>
+                                ))
+                            }
                         </SidebarMenu>
                     </SidebarGroupContent>
                 </SidebarGroup>
