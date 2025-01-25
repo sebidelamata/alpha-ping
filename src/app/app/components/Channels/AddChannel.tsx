8d71ca2436ec4064a6cfb453d1309bad8af3edbb
Miguel  de la Mata <sebidelamata@gmail.com>
2025-01-25 06:28:33 -0500

    cleaned up addModal

--- src/app/app/components/Channels/AddChannel.tsx
+++ src/app/app/components/Channels/AddChannel.tsx
@@ -1,14 +1,8 @@
 'use client';
 
 import React, {
-    useState, 
-    useEffect,
-    useRef,
-    FormEvent
+    useState,
 } from "react";
-import Loading from "../Loading";
-import { useEtherProviderContext } from '../../../../contexts/ProviderContext';
-import { useChannelProviderContext } from "../../../../contexts/ChannelContext";
 import {
     SidebarGroup,
     SidebarGroupContent,
@@ -17,91 +11,13 @@ import {
   import { Plus } from "lucide-react";
 import AddChannelModal from "./AddChannelModal";
 
-interface ErrorType {
-    reason: string
-}
-
 const AddChannel:React.FC = () => {
 
-    const { 
-        alphaPING, 
-        signer, 
-        setChannels 
-    } = useEtherProviderContext()
-    const { 
-            addChannelLoading, 
-            setAddChannelLoading, 
-    } = useChannelProviderContext()
-
     const [showAddChannelModal, setShowAddChannelModal] = useState<boolean>(false)
-    const [tokenAddress, setTokenAddress] = useState<string>("")
-    const [tokenType, setTokenType] = useState<string>("ERC20")
-    const modalRef = useRef<HTMLDivElement>(null);
-    const[error, setError] = useState<string | null>(null)
 
     const addChannelModal = () => {
         setShowAddChannelModal(true)
     }
-
-    const createChannel = async (e:FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        if(tokenAddress === ""){return}
-        if(tokenAddress.length !== 42){return}
-
-        setAddChannelLoading(true)
-
-        try{
-            const transaction = await alphaPING?.connect(signer).createChannel(tokenAddress,tokenType)
-            await transaction?.wait()
-
-            if(alphaPING !== null){
-                const totalChannels:bigint = await alphaPING.totalChannels()
-                const channels = []
-
-                for (let i = 1; i <= Number(totalChannels); i++) {
-                    const channel = await alphaPING.getChannel(i)
-                    channels.push(channel)
-                }
-
-                setChannels(channels)
-            }
-
-            setShowAddChannelModal(false)
-            setTokenAddress("")
-            setTokenType("ERC20")
-            setError(null)
-        }catch(error: unknown){
-            if((error as ErrorType).reason){
-                setError((error as ErrorType).reason)
-            }
-            if(error){
-                console.log(error)
-                // setError(error.toString())
-            }
-        } finally{
-            setTokenAddress("")
-            setTokenType("ERC20")
-            setError(null)
-            setAddChannelLoading(false)
-        }
-
-    }
-
-    // handling closing the modal when clicking outside of it
-    useEffect(() => {
-        const handleClickOutside = (event: MouseEvent) => {
-            if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
-                setShowAddChannelModal(false);
-                setError(null)
-            }
-        };
-
-        document.addEventListener("mousedown", handleClickOutside);
-
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, []);
 
     return(
         <>
@@ -116,55 +32,6 @@ const AddChannel:React.FC = () => {
             {
                 showAddChannelModal === true &&
                 <AddChannelModal/>
-                // <div className="add-channel-greyout">
-                //     <div className="add-channel-modal" ref={modalRef}>
-                //         <h2 className="add-channel-title">
-                //             Add Channel
-                //         </h2>
-                //         <h4 className="add-channel-text">
-                //             Enter the address of any token (ERC-20) or NFT (ERC-721).
-                //             {
-                //                 error !== null &&
-                //                 <h2>{error}</h2>
-                //             }
-                //         </h4>
-                //         <form action="" className="add-channel-form" onSubmit={(e) => createChannel(e)}>
-                //             <div className="form-line-one">
-                //                 <label htmlFor="address">Address</label>
-                //                 <input 
-                //                     type="text" 
-                //                     name="address" 
-                //                     placeholder="0x..." 
-                //                     value={tokenAddress} 
-                //                     onChange={(e) => setTokenAddress(e.target.value)} 
-                //                 />
-                //                 <label htmlFor="type">Token Type</label>
-                //                 <select 
-                //                     className="token-type-selector" 
-                //                     name="type"
-                //                     onChange={(e) => setTokenType(e.target.value)}
-                //                     value={tokenType}
-                //                 >
-                //                     <option value="ERC20">ERC-20</option>
-                //                     <option value="ERC721">ERC-721</option>
-                //                 </select>
-                //             </div>
-                //             <div className="add-channel-button-container">
-                //                 <button 
-                //                     className="add-channel-button"
-                //                     type="submit"
-                //                 >
-                //                     <h3>Create</h3>
-                //                 </button>
-                //             </div>
-                //         </form>
-                //         {
-                //             addChannelLoading &&
-                //             <Loading/>
-                //         }
-                //     </div>
-                // </div>
-
             }
         </>
     )
--- src/app/app/components/Channels/AddChannelModal.tsx
+++ src/app/app/components/Channels/AddChannelModal.tsx
@@ -8,9 +8,94 @@ import {
     DialogHeader,
     DialogTitle,
   } from "@/components/components/ui/dialog"
+import { Button } from "@/components/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/components/ui/form"
+import { Input } from "@/components/components/ui/input"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/components/ui/select"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { useEtherProviderContext } from "../../../../contexts/ProviderContext";
+import { useChannelProviderContext } from "../../../../contexts/ChannelContext";
+
+const formSchema = z.object({
+    tokenAddress: z.string().min(42).max(42),
+    tokenType: z.enum(["ERC20", "ERC721"], {
+        required_error: "Token type is required",
+    }),
+})
+
+type FormValues = z.infer<typeof formSchema>;
+
+interface ErrorType {
+    reason: string
+}
+
+
 
 const AddChannelModal:React.FC = () => {
+
+    const { 
+        alphaPING, 
+        signer, 
+        setChannels 
+    } = useEtherProviderContext()
+    const { 
+            addChannelLoading, 
+            setAddChannelLoading, 
+    } = useChannelProviderContext()
+
+    const [tokenAddress, setTokenAddress] = useState<string>("")
+    const [tokenType, setTokenType] = useState<string>("ERC20")
     const[error, setError] = useState<string | null>(null)
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            tokenAddress: "0x0000000000000000000000000000000000000000",
+            tokenType: "ERC20"
+        },
+    })
+
+    const onSubmit = async (values: FormValues) => {
+        setAddChannelLoading(true);
+    
+        const { tokenAddress, tokenType } = values;
+    
+        try {
+          const transaction = await alphaPING?.connect(signer).createChannel(
+            tokenAddress,
+            tokenType
+          );
+          await transaction?.wait();
+    
+          if (alphaPING !== null) {
+            const totalChannels: bigint = await alphaPING.totalChannels();
+            const channels = [];
+    
+            for (let i = 1; i <= Number(totalChannels); i++) {
+              const channel = await alphaPING.getChannel(i);
+              channels.push(channel);
+            }
+    
+            setChannels(channels);
+          }
+        }
+    }
 
     return(
         <Dialog open={true}>
@@ -20,14 +105,69 @@ const AddChannelModal:React.FC = () => {
                         Add Channel
                     </DialogTitle>
                     <DialogDescription>
-                        Enter the address of any token (ERC-20) or NFT (ERC-721).
+                        <p>Enter the address of any token (ERC-20) or NFT (ERC-721).</p>
                         {
                             error !== null &&
                             <h2>{error}</h2>
                         }
+                        <Form {...form}>
+                            <form 
+                                onSubmit={form.handleSubmit(onSubmit)} 
+                                className="space-y-8"
+                            >
+                                <FormField
+                                    control={form.control}
+                                    name="tokenAddress"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Address</FormLabel>
+                                            <FormControl>
+                                                <Input 
+                                                    placeholder="0x0000000000000000000000000000000000000000"
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="tokenType"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Token Type</FormLabel>
+                                            <FormControl>
+                                                <Select>
+                                                    <SelectTrigger className="w-[180px]">
+                                                        <SelectValue placeholder="ERC-20" />
+                                                        <SelectContent>
+                                                            <SelectItem 
+                                                                value="ERC20"
+                                                            >
+                                                                ERC-20
+                                                            </SelectItem>
+                                                            <SelectItem 
+                                                                value="ERC721"
+                                                            >
+                                                                ERC-721
+                                                            </SelectItem>
+                                                        </SelectContent>
+                                                    </SelectTrigger>
+                                                </Select>
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
+                                <Button type="submit">Submit</Button>
+                            </form>
+                        </Form>
                     </DialogDescription>
                 </DialogHeader>
             </DialogContent>
+            {
+                addChannelLoading === true &&
+                <Loading/>
+            }
         </Dialog>
     )
 }
