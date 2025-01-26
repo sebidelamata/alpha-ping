6ab1a0bd1abb629deaa769c43bab107943e4f995
Miguel  de la Mata <sebidelamata@gmail.com>
2025-01-26 10:28:29 -0500

    sidebar footer button

--- src/app/app/components/Profile/SidebarProfileFooter.tsx
+++ src/app/app/components/Profile/SidebarProfileFooter.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { SidebarFooter } from "@/components/components/ui/sidebar";
+import { 
+    SidebarFooter,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem 
+} from "@/components/components/ui/sidebar";
 import { 
     Avatar,
     AvatarImage
@@ -8,32 +13,50 @@ import { useUserProviderContext } from "src/contexts/UserContext";
 
 const SidebarProfileFooter:React.FC = () => {
 
-    const { userProfilePic } = useUserProviderContext()
+    const { 
+        userProfilePic, 
+        userUsername 
+    } = useUserProviderContext()
 
     return(
-        <SidebarFooter className="flex flex-row items-center justify-start bg-primary text-secondary">
-        {
-            (
-                userProfilePic !== null &&
-                userProfilePic !== "" &&
-                userProfilePic !== undefined
-            ) ?
-            <Avatar>
-                <AvatarImage 
-                src={userProfilePic} 
-                alt="user profile picture" 
-                loading="lazy" 
-                />
-            </Avatar> :
-            <Avatar>
-                <AvatarImage 
-                src="/monkey.svg" 
-                alt="default profile picture" 
-                loading="lazy"
-                />
-            </Avatar>
-        }
-            Profile
+        <SidebarFooter
+            className="bg-primary text-accent"
+        >
+            <SidebarMenu>
+                <SidebarMenuItem 
+                    className="flex flex-row items-center justify-start gap-4 overflow-hidden"
+                >
+                    <SidebarMenuButton
+                    >
+                        {
+                            (
+                                userProfilePic !== null &&
+                                userProfilePic !== "" &&
+                                userProfilePic !== undefined
+                            ) ?
+                            <Avatar>
+                                <AvatarImage 
+                                src={userProfilePic} 
+                                alt="user profile picture" 
+                                loading="lazy" 
+                                />
+                            </Avatar> :
+                            <Avatar>
+                                <AvatarImage 
+                                src="/monkey.svg" 
+                                alt="default profile picture" 
+                                loading="lazy"
+                                />
+                            </Avatar>
+                        }
+                        <h2
+                            className="text-lg"
+                        >
+                            {userUsername}
+                        </h2>
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
+            </SidebarMenu>
         </SidebarFooter>
     )
 }
