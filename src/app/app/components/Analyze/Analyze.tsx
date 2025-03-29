269ea527f3076b6e69c35e5d8d64e05c8e4a9578
Miguel  de la Mata <sebidelamata@gmail.com>
2025-03-29 14:08:36 -0400

    messagescore

--- src/app/app/components/Analyze/Analyze.tsx
+++ src/app/app/components/Analyze/Analyze.tsx
@@ -8,36 +8,60 @@ import vader from 'vader-sentiment'
 import { useChannelProviderContext } from "src/contexts/ChannelContext";
 import { useMessagesProviderContext } from "src/contexts/MessagesContext";
 import { mockMessages } from "mocks/mockMessages";
+import { 
+    Card, 
+    CardHeader, 
+    CardTitle, 
+    CardDescription, 
+    CardContent, 
+    CardFooter 
+} from "@/components/components/ui/card";
 
 const Analyze:React.FC = () => {
 
-    const { selectedChannelMetadata } = useChannelProviderContext()
+    const { 
+        selectedChannelMetadata, 
+        currentChannel 
+    } = useChannelProviderContext()
     const { messages } = useMessagesProviderContext()
     
-    const [channelScore, setChannelScore] = useState<null | number>(null)
+    const [allMessagesScore, setallMessagesScore] = useState<null | number>(null)
 
     useEffect(() => {
-        const getCompoundMessagesScore = () => {
+        const getAllMessagesScore = () => {
             const input = mockMessages.map((message) => {
                 return message.text
             })
             .join()
             console.log(input)
             const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(input);
-            setChannelScore(intensity.compound)
+            setallMessagesScore(intensity.compound)
             console.log(intensity);
         }
-        getCompoundMessagesScore()
-    }, [])
+        getAllMessagesScore()
+    }, [messages])
 
     return(
-        <div>
-            Analyze
-            {
-                channelScore !== null &&
-                <p>{channelScore}</p>
-            }
-        </div>
+        <Card
+        className="flex flex-col w-full h-full bg-primary text-secondary overflow-clip"
+        onWheel={(e) => {
+        e.stopPropagation(); 
+      }}
+        >
+            <CardHeader>
+                <CardTitle>
+                    Analyze
+                </CardTitle>
+            </CardHeader>
+            <CardContent
+                className='flex-1 h-full w-full'
+            >
+                {
+                    allMessagesScore !== null &&
+                    <p>{allMessagesScore}</p>
+                }
+            </CardContent>
+        </Card>
     )
 }
 
