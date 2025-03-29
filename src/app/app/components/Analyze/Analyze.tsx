1c6f307c01c772dfe02c6595a31ec90d911ecdca
Miguel  de la Mata <sebidelamata@gmail.com>
2025-03-29 07:38:51 -0400

    basic messages score function

--- src/app/app/components/Analyze/Analyze.tsx
+++ src/app/app/components/Analyze/Analyze.tsx
@@ -1,16 +1,42 @@
 'use client';
 
-import React from "react";
+import React, {
+    useEffect,
+    useState
+} from "react";
 import vader from 'vader-sentiment'
-
-const input = 'VADER is very smart, handsome, and funny';
-const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(input);
-console.log(intensity);
+import { useChannelProviderContext } from "src/contexts/ChannelContext";
+import { useMessagesProviderContext } from "src/contexts/MessagesContext";
+import { mockMessages } from "mocks/mockMessages";
 
 const Analyze:React.FC = () => {
+
+    const { selectedChannelMetadata } = useChannelProviderContext()
+    const { messages } = useMessagesProviderContext()
+    
+    const [channelScore, setChannelScore] = useState<null | number>(null)
+
+    useEffect(() => {
+        const getCompoundMessagesScore = () => {
+            const input = mockMessages.map((message) => {
+                return message.text
+            })
+            .join()
+            console.log(input)
+            const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(input);
+            setChannelScore(intensity.compound)
+            console.log(intensity);
+        }
+        getCompoundMessagesScore()
+    }, [])
+
     return(
         <div>
             Analyze
+            {
+                channelScore !== null &&
+                <p>{channelScore}</p>
+            }
         </div>
     )
 }
