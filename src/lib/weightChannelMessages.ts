777be603f699cc1adc123d9cc79809f595556f71
Miguel  de la Mata <sebidelamata@gmail.com>
2025-04-20 20:00:45 -0400

    abstract weighting

--- src/lib/weightChannelMessages.ts
+++ src/lib/weightChannelMessages.ts
@@ -4,34 +4,33 @@ const weightChannelMessages = (
     messages:Message[], 
     messageWeighting: Weighting, 
     currentChannel: AlphaPING.ChannelStructOutput
-):Message[] => {
-    if(messages.length <= 0){
-        return messages
+):number[] => {
+    if(messages.length === 0){
+        return []
     }
     const channelMessages = messages.filter((message) => {
         return message.channel.toString() === currentChannel.id.toString()
     })
     if(messageWeighting === "unweighted"){
-        return channelMessages
-    } else {
+        // we return full weighting * by 1 (or return empty array)
+        const identityWeights = Array(messages.length).fill(1) || []
+        return identityWeights
+    } else if(messageWeighting === "post"){
         // find total for avg calc, if its undefined just make it zero
         const total = channelMessages.reduce((sum, message) => sum + BigInt(message.messageTimestampTokenAmount), BigInt(0)) || BigInt(0)
-        const weightedChannelMessages = channelMessages.map((message) => {
-                if (messageWeighting === "post") {
-                    return {
-                        ...message,
-                        weighting: total !== undefined
-                            // fallback to 0 in case we divide by zero or divide zero or anything weird
-                            ? (Number(message.messageTimestampTokenAmount) / Number(total) * 100)
-                                .toFixed(2)
-                                .toString() || "0"
-                            : null
-                    }
-                }
-                return message
-             }
+        const weights = channelMessages.map((message) => {
+                return (
+                    // fallback to 0 in case we divide by zero or divide zero or anything weird
+                    Number(
+                        (Number(message.messageTimestampTokenAmount) / Number(total) * 100)
+                        .toFixed(2) || 0 
+                    )
+                )
+            }
         )
-        return weightedChannelMessages
+        return weights
+    } else {
+        return []
     }
 }
 
