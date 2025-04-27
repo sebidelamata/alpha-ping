d29b4bdf6b2535a0ba3752fdb01d647a111d4079
Miguel  de la Mata <sebidelamata@gmail.com>
2025-04-27 14:45:58 -0400

    inverse post balance added

--- src/lib/weightAllMessages.ts
+++ src/lib/weightAllMessages.ts
@@ -28,7 +28,28 @@ const weightAllMessages = (
             }
         )
         return weights
-    } else {
+    } else if(messageWeighting === "inverse"){
+        // find total for avg calc, if its undefined just make it zero
+        const total = messages.reduce((sum, message) => sum + BigInt(message.messageTimestampTokenAmount), BigInt(0)) || BigInt(0)
+        // if the total is zero everything is zero and therefore we weight them all 100%
+        if (total === BigInt(0)) {
+            // fallback to 1 weights to prevent NaN
+            return Array(messages.length).fill(1);
+        }
+        const weights = messages.map((message) => {
+                return (
+                    // inverse percent is 1 - weight eg 0.1 = 1 - 0.9
+                    // fallback to 1 in case we divide by zero or divide zero or anything weird
+                    Number(
+                        (1 - (Number(message.messageTimestampTokenAmount) / Number(total)))
+                        .toFixed(2) || 1 
+                    )
+                )
+            }
+        )
+        return weights
+    }
+     else {
         return []
     }
 }
