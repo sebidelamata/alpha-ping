643c9771898fc694a7859dca874d05094915752f
Miguel  de la Mata <sebidelamata@gmail.com>
2025-06-05 05:21:36 -0400

    docs type

--- src/app/docs/components/Overview.tsx
+++ src/app/docs/components/Overview.tsx
@@ -9,7 +9,7 @@ const Overview:React.FC = () => {
             </h2>
             <div className="grow p-4 pt-16">
                 <p>
-                    AlphaPING is an aggregated social trading platform that combines real-time chat, data analysis, project research, and trading in a single application. It is designed to foster a transparent and engaging community of traders, researchers, and enthusiasts, while filtering out spam and bots. Users gain access by minting a ERC-721 membership token. Token holdings and balances are recorded and displayed alongside each user’s posts, helping establish transparency and discouraging fraudulent activities. Integrated with the 1Inch Router, AlphaPING ensures users can execute trades at optimal rates without needing to exit the platform.
+                    AlphaPING is an aggregated social trading platform that combines real-time chat, data analysis, project research, and trading in a single application. It is designed to foster a transparent and engaging community of traders, researchers, and enthusiasts, while filtering out spam and bots. Users gain access by minting a ERC-721 membership token. Token holdings and balances are recorded and displayed alongside each user’s posts, helping establish transparency and discouraging fraudulent activities. Integrated with the 0x RFQ protocol. AlphaPING ensures users can execute trades at optimal rates without needing to exit the platform.
                 </p>
             </div>
             <Footer/>
