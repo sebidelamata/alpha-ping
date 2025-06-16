fde1f9c1dfcf89ff553106ab8e3f0336fd2a0a1e
Miguel  de la Mata <sebidelamata@gmail.com>
2025-06-16 08:28:54 -0400

    path error

--- next.config.ts
+++ next.config.ts
@@ -3,3 +3,14 @@ module.exports = {
     domains: ['ipfs.io'],
   },
 };
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  webpack: config => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    return config
+  }
+};
+
+export default nextConfig;
