945d578b349eb4ab4e72647b010ba85f2560fadd
Miguel  de la Mata <sebidelamata@gmail.com>
2025-07-31 15:25:01 -0400

    coin gecko id slug hook

--- src/hooks/useGetCoinGeckoID.ts
+++ src/hooks/useGetCoinGeckoID.ts
@@ -3,12 +3,20 @@ import { useChannelProviderContext } from 'src/contexts/ChannelContext';
 
 const useGetCoinGeckoID = () => {
 
-    const { selectedChannelMetadata } = useChannelProviderContext()
+    const { selectedChannelMetadata, currentChannel } = useChannelProviderContext()
     
     const coinGeckoMetadata = coinGeckoIdMap.filter((token) => {
         if(selectedChannelMetadata && selectedChannelMetadata.platform.slug === 'ethereum' && token.platforms['ethereum']) {
-        return token.platforms['ethereum'].toLowerCase() === (selectedChannelMetadata.platform.token_address).toLowerCase();
-    }})
+            return token.platforms['ethereum'].toLowerCase() === (selectedChannelMetadata.platform.token_address).toLowerCase()
+        }
+        if(selectedChannelMetadata && selectedChannelMetadata.platform.slug === 'arbitrum' && token.platforms['arbitrum-one']) {
+            return token.platforms['arbitrum-one'].toLowerCase() === (selectedChannelMetadata.platform.token_address).toLowerCase()
+        }
+        // nfts or small tokens wont have cmc token metadata but might have a token address
+        if(!selectedChannelMetadata && currentChannel?.tokenAddress) {
+            return token.platforms['arbitrum-one'].toLowerCase() === (currentChannel.tokenAddress).toLowerCase()
+        }
+    })
 
     const coinGeckoId = coinGeckoMetadata.length > 0 ? coinGeckoMetadata[0].id : null;
 
