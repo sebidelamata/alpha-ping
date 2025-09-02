e2cd4a382b75a8a1f90279bad04d4d6ddea1b687
Miguel  de la Mata <sebidelamata@gmail.com>
2025-09-01 22:10:18 -0400

    hotfix

--- src/app/app/components/Trade/SellTokenUSDCustomTooltip.tsx
+++ src/app/app/components/Trade/SellTokenUSDCustomTooltip.tsx
@@ -25,10 +25,10 @@ const SellTokenUSDCustomTooltip: React.FC<ICustomTooltipProps> = ({
         {/* Market data */}
         {data.price !== undefined && <p>{`Price: ${data.price.toFixed(2)} ${sellTokenObject?.symbol}`}</p>}
         {data.market_cap !== undefined && (
-          <p>{`Market Cap: ${(data.market_cap / 1e9).toFixed(2)} of ${sellTokenObject?.symbol}`}</p>
+          <p>{`Market Cap: ${(data.market_cap).toFixed(2)} of ${sellTokenObject?.symbol}`}</p>
         )}
         {data.volume !== undefined && (
-          <p>{`Volume: ${(data.volume / 1e9).toFixed(2)} of ${sellTokenObject?.symbol}`}</p>
+          <p>{`Volume: ${(data.volume).toFixed(2)} of ${sellTokenObject?.symbol}`}</p>
         )}
 
       </div>
