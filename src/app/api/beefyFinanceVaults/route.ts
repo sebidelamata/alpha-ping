4803138c92e93148a509be84ecd5270a090fbc2b
Miguel  de la Mata <sebidelamata@gmail.com>
2025-09-02 19:36:49 -0400

    beefy vaults api route

---
+++ src/app/api/beefyFinanceVaults/route.ts
@@ -0,0 +1,19 @@
+
+export async function GET() {
+  
+  try {
+    const res = await fetch(
+      `https://api.beefy.finance/vaults/`,
+      {
+        headers: {
+        'Accepts': 'application/json',
+        },
+      }
+    );
+    const data = await res.json();
+
+    return Response.json(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
