331655582182072576c71886c9a02cac67129b0e
Miguel  de la Mata <sebidelamata@gmail.com>
2025-09-23 06:38:43 -0400

    switch over for hotfix

--- src/app/app/components/Messages/MessagesHeader/MessagesHeaderBeefyStats.tsx
+++ src/app/app/components/Messages/MessagesHeader/MessagesHeaderBeefyStats.tsx
@@ -1,9 +1,6 @@
 'use client';
 
-import React, { 
-    useState, 
-    useEffect 
-} from "react";
+import React from "react";
 import Link from "next/link";
 import { 
     Avatar, 
@@ -11,86 +8,18 @@ import {
     AvatarImage 
 } from "@/components/components/ui/avatar";
 import { useChannelProviderContext } from "src/contexts/ChannelContext";
-import { useAaveDetailsContext } from "src/contexts/AaveDetailsContext";
-import { useEtherProviderContext } from "src/contexts/ProviderContext";
 import humanReadableNumbers from "src/lib/humanReadableNumbers";
 import { Separator } from "@/components/components/ui/separator";
-import AaveL2LendingPool from '../../../../../lib/aaveL2PoolABI.json'
-import aTokenUnderlyingAsset from '../../../../../lib/aTokenAaveUnderlyingAsset.json'
-import { 
-  ethers, 
-  formatUnits 
-} from 'ethers';
 import { Badge } from "@/components/components/ui/badge";
 
 const MessagesHeaderAaveStats:React.FC = () => {
 
     const { 
         selectedChannelMetadata,
-        currentChannel 
     } = useChannelProviderContext()
-    const { aaveAccount } = useAaveDetailsContext()
-    const { signer } = useEtherProviderContext()
-
-    // this fetches the supply and borrow APY for the token
-    const [aaveAssetDetails, setAaveAssetDetails] = useState<AaveSupplyBorrowRate | null>(null)
-    useEffect(() => {
-        if(!currentChannel){
-            return
-        }
-        const fetchAaveDetails = async () => {
-            const aaveLendingPool = new ethers.Contract(
-            // aave lending pool address
-            "0x794a61358d6845594f94dc1db02a252b5b4814ad",
-            AaveL2LendingPool.abi,
-            signer
-            );
-            const aToken = new ethers.Contract(
-                currentChannel?.tokenAddress,
-                aTokenUnderlyingAsset.abi,
-                signer
-            );
-          try{
-            const underlyingAssetAddress = await aToken.UNDERLYING_ASSET_ADDRESS()
-            const reserveData = await aaveLendingPool.getReserveData(underlyingAssetAddress)
-            if (reserveData) {
-              console.log('reserveData: ', reserveData)
-    
-              // Raw values are BigNumbers; convert them to human‑readable strings
-              const cleanedReservesData: AaveSupplyBorrowRate = {
-                supplyRate: formatUnits(reserveData.currentLiquidityRate, 27),
-                borrowRate: formatUnits(reserveData.currentVariableBorrowRate, 27)
-              };
-              console.log('asset aave data:', cleanedReservesData);
-              setAaveAssetDetails(cleanedReservesData);
-            } else {
-                console.warn('No aave account data found for this asset:', underlyingAssetAddress);
-                return;
-            }
-          } catch(error: unknown){
-            if(error !== undefined || error !== null){
-                console.warn("Error unable to fetch aave user account details for: " + signer + ": " + (error as Error).toString())
-                return;
-            }
-          }
-        }
-    
-        // only run this function if the user is part of an aave channel
-        if(
-            selectedChannelMetadata &&
-            selectedChannelMetadata.protocol &&
-            selectedChannelMetadata.protocol === 'aave'
-        ){
-          fetchAaveDetails()
-        }
-      }, [
-        currentChannel,
-        selectedChannelMetadata,
-        signer
-      ])
+ 
 
     if(
-        aaveAccount !== null &&
         selectedChannelMetadata !== null &&
         selectedChannelMetadata.protocol &&
         selectedChannelMetadata.protocol === 'aave'
